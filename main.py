77ca0b94b0411b8ef1f28bb78e0539b7e0d0ffa2
heysmtk <smtktom@gmail.com>
2025-03-31 23:16:59 +0200

    edited functions and main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+from stats import count_words, count_chars
+
+def get_book_text(filepath):
+    """Load and read the file"""
+    with open(filepath, "r") as file:
+        content = file.read()
+    return content
+
+
+text = get_book_text("books/frankenstein.txt")
+
+number_of_words = count_words(text)
+print(f"{number_of_words} words found in the document")
+
+number_of_characters = count_chars(text)
+print(number_of_characters)
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def count_words(book):
     num_words = len(book.split())
     return num_words
+
+
+def count_chars(content):
+    char_num = {}  # Prázdný slovník pro počítání znaků
+
+    for char in content:  # Procházíme každý znak přímo
+        char = char.lower()  # Převedeme na malá písmena
+        char_num[char] = char_num.get(char, 0) + 1  # Zvýšíme počet výskytů
+
+    return char_num
