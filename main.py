5018289e40888a10c7372a67de14f8dcd481b537
heysmtk <smtktom@gmail.com>
2025-04-02 22:47:51 +0200

    add sys.argv for arguments in CLI

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from stats import count_words, count_chars, sorted_chars
+import sys
+
+# Setting for CLI arguments
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 
 def get_book_text(filepath):
     """Load and read the file"""
-    with open(filepath, "r") as file:
-        content = file.read()
-    return content
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        print(f"Error: File '{filepath}' not found!")
+        sys.exit(1)
 
 
 # Setting to variables
-filepath = "frankenstein.txt"
-text = get_book_text(f"books/{filepath}")
+filepath = sys.argv[1]
+text = get_book_text(filepath)
 number_of_words = count_words(text)
 number_of_characters = count_chars(text)
 sorted_characters = sorted_chars(number_of_characters)
